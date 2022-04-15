0679655a429653fd6e7710f16883a8f8e88db799
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2022-04-15 23:37:44 +0300

    Implement core namespace for running the application

--- src/waddams/core.clj
+++ src/waddams/core.clj
@@ -1,4 +1,32 @@
-(ns waddams.core)
+(ns waddams.core
+  (:require [clojure.java.io :as io]
+            [waddams.csv :as csv]
+            [waddams.transaction :as transaction]))
+
+(def all-transactions-file-path "waddams-output/all-transactions.csv")
+
+(defn write-completed-ticker-transactions! [completed-grouped-transactions]
+  (doseq [ticker (keys completed-grouped-transactions)
+          :let [file-path (format "waddams-output/%s.csv" ticker)]]
+    (println (format "Writing %s" file-path))
+    (->> (get completed-grouped-transactions ticker)
+         (csv/write-completed-transactions! file-path))))
+
+(defn write-all-transactions! [completed-combined-transactions]
+  (println (format "Writing %s" all-transactions-file-path))
+  (csv/write-completed-transactions! all-transactions-file-path
+                                     completed-combined-transactions))
 
 (defn -main [& args]
-  (println "HELLO WORLD!"))
+  (with-precision 10
+    (let [{:keys [close! transactions]} (-> args first csv/read-transactions)
+          completed-grouped-transactions
+          (transaction/completed-grouped-transactions transactions)
+          completed-combined-transactions (->> completed-grouped-transactions
+                                               vals
+                                               (apply concat)
+                                               (sort-by :ts))]
+      (io/make-parents all-transactions-file-path)
+      (write-completed-ticker-transactions! completed-grouped-transactions)
+      (write-all-transactions! completed-combined-transactions)
+      (close!))))
